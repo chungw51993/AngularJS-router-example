0a5b4f734ffdcc45ba6500ecf464f60fc5724b5e
Won Jin Chung <chungw51993@gmail.com>
2017-07-15 11:39:07 -0500

    Added comments to the files

--- client/app.js
+++ client/app.js
@@ -1,9 +1,13 @@
 angular.module('routeExample', [
+  // You'll have to add ngRoute as a dependency at the root level of you app
   'ngRoute',
 ])
 
 .config(function($routeProvider) {
-
+  // Define your routes using $routeProvider
+  // First parameter of when accepts your route definition
+  // Second parameter, you are mapping information to be assigned to current route
+  // Otherwise sets route definition that will be used on route change if no route is matched
   $routeProvider
     .when('/route1', {
       templateUrl: 'client/route1.html'
--- client/route3.js
+++ client/route3.js
@@ -1,5 +1,6 @@
 angular.module('routeExample')
 
 .controller('Route3Ctrl', function($scope, $routeParams) {
+  // You can access the url params by injecting $routeParams to your controller
   $scope.heading = 'id in the url params is ' + $routeParams.id;
 });
